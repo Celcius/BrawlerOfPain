7b6f9288263f57ce6fa0550450447a99415d0c67
Bruno Santos <bmfs@eksperimental.net>
2014-10-24 23:49:00 +0100

    Added .DS_Store to gitignore

--- project/Assets/Scripts/Player.cs
+++ project/Assets/Scripts/Player.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float speed = 10f;
+	private PlayerController _inputController;
+	private Vector3 currentPosition;
 	// Use this for initialization
 	void Start () {
-	
+		_inputController = GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		handleMovement ();
+	}
+
+	private void handleMovement()
+	{
+		if (_inputController.inputDevice == null) return;
+
+		currentPosition = transform.position;
+		currentPosition.x += speed * Time.deltaTime * _inputController.inputDevice.Direction.X;
+		currentPosition.z += speed * Time.deltaTime * _inputController.inputDevice.Direction.Y;
+		transform.position = currentPosition;
 	}
 }

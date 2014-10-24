52eff835c37e689844bc45cf43d9b43262ebcc83
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2014-10-25 00:22:36 +0100

    First tile map working

---
+++ project/Assets/Scripts/GridElement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridElement : MonoBehaviour {
+
+    public const string FLOOR_CODE = "f";
+    public const string HOLE_CODE = "o";
+
+    protected const float unityScale = 10.0f;
+
+    protected int _gridPosX;
+    protected int _gridPosY;
+
+    public void setGridElement(int x, int y) 
+    {
+        _gridPosX = x;
+        _gridPosY = y;
+        this.gameObject.transform.position = new Vector3(_gridPosX, 0, _gridPosY);
+      //  this.gameObject.transform.localScale = new Vector3(1 / unityScale, 1 / unityScale, 1 / unityScale); // use this for planes
+    }
+	
+    void Start()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    public static void createGridElement(string code, int x, int y)
+    {
+        if (FLOOR_CODE.CompareTo(code) == 0)
+        {
+            createPlaneFloorGridElement(x , y);
+        }
+        else if( HOLE_CODE.CompareTo(code)== 0)
+        {
+            createHoleGridElement(x, y);
+        }
+;
+    }
+
+    static void createPlaneFloorGridElement(int x, int y)
+    {
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        plane.AddComponent<BoxCollider>();
+        plane.AddComponent<GridElement>().setGridElement(x, y); 
+    }    
+
+    static void  createHoleGridElement(int x, int y)
+    {
+
+    }
+
+}
+
---
+++ project/Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,58 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+    
+    [SerializeField]
+    private const int MAP_WIDTH = 20;
+    [SerializeField]
+    private const int MAP_HEIGHT = 20;
+    [SerializeField]
+    private int HOLE_MARGIN = 2;
+
+    string[,] map = new string[MAP_WIDTH, MAP_HEIGHT];
+                   
+	// Use this for initialization
+	void Start () {
+        initializeGrid();
+        setupGrid();
+        setupDeathColider();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void initializeGrid()
+    {
+        for(int x = 0; x < MAP_WIDTH; x++)
+            for(int y = 0; y < MAP_WIDTH; y++)
+            {
+                if(x < HOLE_MARGIN || x > MAP_WIDTH-HOLE_MARGIN ||
+                    y < HOLE_MARGIN || y > MAP_HEIGHT-HOLE_MARGIN)
+                {
+                    map[x,y] = GridElement.HOLE_CODE;
+                }
+                else
+                    map[x, y] = GridElement.FLOOR_CODE;
+            }
+    }
+    void setupGrid()
+    {
+        for(int x = 0; x < MAP_WIDTH; x++)
+            for(int y = 0; y < MAP_HEIGHT; y++)
+            {
+                string gridCode = map[x, y];
+                GridElement.createGridElement(gridCode, x, y);
+            }
+    }
+
+    void setupDeathColider()
+    {
+
+    }
+}
+

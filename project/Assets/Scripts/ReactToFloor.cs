100edd3ff14f24b92f5a914dbcb465ed91b7b862
Bruno Santos <bmfs@eksperimental.net>
2014-10-25 19:42:57 +0100

    Changed player properties for mapCreation level

---
+++ project/Assets/Scripts/ReactToFloor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactToFloor : MonoBehaviour {
+	
+	private CharacterMotor motor;
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent<CharacterMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetFloorPhysics();
+	}
+	
+	private void SetFloorPhysics()
+	{
+		GridElement cell = GameManager.instance.getCellAtWorldPosition(transform.position.x, transform.position.z);
+		//Debug.Log ("current cell bloody:" + cell._isBloodied);
+		if (cell._isBloodied){
+			motor.movement.maxGroundAcceleration = 10;
+		} else {
+			motor.movement.maxGroundAcceleration = 100;
+		}
+	}
+}

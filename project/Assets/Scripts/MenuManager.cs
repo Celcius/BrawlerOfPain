a7e9135ff11c518d5a547851539b5988fba1a247
gaps <gaps@noemail.pt>
2014-10-25 21:09:53 +0100

    GUI skeleton

---
+++ project/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject MainScreen;
+    public GameObject OptionsScreen;
+    public GameObject EndScreen;
+
+    void Start()
+    {
+        OptionsScreen.SetActive(false);
+        EndScreen.SetActive(false);
+    }
+	
+}

340e05f16f268642529aeffb5efbe948eaa9192d
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2014-10-26 17:52:04 +0000

    brazer

---
+++ project/Assets/Scripts/Flicker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+    Light _light;
+    float minIntensity = 0.5f;
+	// Use this for initialization
+	void Start () {
+        _light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float diff = Random.Range(-3,3)/100.0f;
+        float newIntensity = _light.intensity + diff;
+        float actualnew = Mathf.Clamp(newIntensity, minIntensity, 1);
+        _light.intensity = actualnew;
+	}
+}
